abb546e97e993ac94a0be4304a5de530e2a15342
Sridharan T <51164940+Sridharan15@users.noreply.github.com>
2020-08-27 12:47:23 +0530

    Update nextDay.swift

--- nextDay.swift
+++ nextDay.swift
@@ -1,18 +1,28 @@
-import UIKit
-var schedule = "WED:WK5"
-let weeks = ["WK1", "WK2", "WK3", "WK4", "WK5"]
-let normalWeeksOrder = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+import Foundation
+var schedule = "TUE:WK1"
+let weeks = ["WK1", "WK2", "WK3", "WK4", "WK5", "WK6"]
+let normalWeeksOrder = ["SUN","MON", "TUE", "WED", "THU", "FRI", "SAT"]
 var weekOrder = [[String]]()
 var weekNameOrder = [String]()
 var valueToAdded = 0
+
 let date = Date()
 let today = DateFormatter()
 today.dateFormat = "EEE"
+var currentDay = today.string(from: date).uppercased()
+today.dateFormat = "yyyy"
+var currentYear = Int(today.string(from: date))!
+print(currentYear)
+today.dateFormat = "MM"
+var currentMonth = Int(today.string(from: date))!
+today.dateFormat = "dd"
+var currentDate = Int(today.string(from: date))!
+
 let calendar = NSCalendar.current
 let weekOfMonth = calendar.component(.weekOfMonth, from: date)
-var currentWeek = "WK5"
-var currentDay = today.string(from: date).uppercased()
+var currentWeek = "WK\(weekOfMonth)"
 var schedulePerWeek = schedule.components(separatedBy: ";")
+
 for perWeek in schedulePerWeek {
     var perWeekSchedule = perWeek.components(separatedBy: ",")
     let a = perWeekSchedule[0].components(separatedBy: ":")
@@ -24,7 +34,7 @@ for perWeek in schedulePerWeek {
     weekOrder.append(perWeekSchedule)
     weekNameOrder.append(a[0])
 }
-makingOrder()
+
 func makingOrder() {
     var tempWeekOrder = [[String]]()
     var tempWeekNameOrder = [String]()
@@ -41,46 +51,77 @@ func makingOrder() {
     weekNameOrder = tempWeekNameOrder
     weekOrder = tempWeekOrder
 }
+makingOrder()
+print(weeks,"WEEKS")
 print(weekOrder, "WEEKORDER")
 print(weekNameOrder, "WEEKNAMEORDER")
-
-var curWeek = weeks.index(of: currentWeek)!
-var nextDay = 0
-if  weekOrder.count != 1{
-    nextDay = normalWeeksOrder.index(of: currentDay)! + 1
-    print(nextDay)
-    if weekOrder.count == nextDay {
-        nextDay = 0
-    }
-} else {
-    nextDay = 0
-    currentWeek = weeks[curWeek]
-    //valueToAdded = -3
-}
-while true {
-    //print(weekOrder[nextDay],nextDay, currentWeek)
-    if weekOrder[nextDay].contains(currentWeek) {
-        print(currentWeek)
-        print(weekOrder[nextDay])
-        print(weekNameOrder[nextDay])
-        valueToAdded = valueToAdded + 1
-        break
-    } else {
-        valueToAdded = valueToAdded + 1
-        if weekNameOrder.count - 1 != nextDay {
-            nextDay = nextDay + 1
-        } else {
-            nextDay = 0
-            if curWeek != weeks.count - 1  {
-                curWeek = curWeek + 1
-                currentWeek = weeks[curWeek]
-            } else {
-                curWeek = 0
-                currentWeek = weeks[curWeek]
+func checkLeapYear(for year: Int) -> Bool {
+    if year % 4 == 0 {
+        if year % 100 == 0 {
+            if year % 400 == 0 {
+                return true
             }
+            else {
+                return false
+            }
+        }
+        else {
+            return true
+        }
+    }
+    else {
+        return false
+    }
+}
+
+func numberOfDaysForEachMonthInCurrentYear(for isLeapYear: Bool) -> [Int] {
+    if isLeapYear == false {
+        return [31,28,31,30,31,30,31,31,30,31,30,31]
+    } 
+    else {
+        return [31,29,31,30,31,30,31,31,30,31,30,31]
+    }
+}
+
+let isLeapYear: Bool = checkLeapYear(for: currentYear)
+var numberOfdaysForEachMonth: [Int] = numberOfDaysForEachMonthInCurrentYear(for: isLeapYear)
+
+var curWeek = weeks.index(of: currentWeek)! //4
+var nextDay = normalWeeksOrder.index(of: currentDay)! + 1 //5
+print(curWeek,nextDay)
+while true {
+    if currentDate == numberOfdaysForEachMonth[currentMonth - 1] {
+        currentMonth += 1
+        curWeek = 0
+        nextDay += 1
+        currentDate = 1
+        print(nextDay)
+    }
+    else {
+        if weekOrder[nextDay].contains(weeks[curWeek]) {
+            valueToAdded += 1
+            currentDate += 1
+            break
+        }
+        else if nextDay != normalWeeksOrder.count - 1 {
+            valueToAdded += 1
+            currentDate += 1
+            nextDay += 1
+        }
+        else if curWeek != weeks.count - 1 {
+            valueToAdded += 1
+            currentDate += 1
+            curWeek += 1
+            nextDay = 0
+        }
+        else {
+            valueToAdded += 1
+            currentDate += 1
+            curWeek = 0 
+            nextDay = 0
         }
     }
 }
 print(valueToAdded)
-let nextTripDay = Calendar.current.date(byAdding: .day, value: valueToAdded, to: date)!
-print(nextTripDay)
+let nextDayVisit = Calendar.current.date(byAdding: .day, value: valueToAdded, to: date)!
+print(nextDayVisit)
