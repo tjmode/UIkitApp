40a8f9842e71d01eef30e39970cacb7be67adff7
Sridharan T <51164940+Sridharan15@users.noreply.github.com>
2020-10-12 10:28:40 +0530

    sample tblViewCell

---
+++ SampleTableViewaCell/SampleTableViewaCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleTableViewaCell
+//
+//  Created by Sridharan T on 10/10/2020.
+//  Copyright © 2020 Sridharan T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
